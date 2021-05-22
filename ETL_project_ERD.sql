98badb59d7e29ad9e94395f46e4e9e08ffd0c0c3
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-05-22 11:21:17 -0400

    file update

--- ETL_project_ERD.sql
+++ ETL_project_ERD.sql
@@ -1,8 +1,6 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/LU6zqp
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-DROP TABLE IF EXISTS dogecoin CASCADE;
-DROP TABLE IF EXISTS dogeusd CASCADE;
 
 CREATE TABLE "dogecoin" (
     "date" Date   NOT NULL,
@@ -10,10 +8,10 @@ CREATE TABLE "dogecoin" (
     "high" Float   NOT NULL,
     "low" Float   NOT NULL,
     "close" Float   NOT NULL,
-    "volume" INT   NOT NULL,
+    "volume" BIGINT   NOT NULL,
     "market_cap" Float   NOT NULL,
     CONSTRAINT "pk_dogecoin" PRIMARY KEY (
-        "date","open","high","low","close","volume"
+        "date"
      )
 );
 
@@ -24,15 +22,8 @@ CREATE TABLE "dogeusd" (
     "low" Float   NOT NULL,
     "close" Float   NOT NULL,
     "adj_close" Float   NOT NULL,
-    "volume" INT   NOT NULL,
+    "volume" BIGINT   NOT NULL,
     CONSTRAINT "pk_dogeusd" PRIMARY KEY (
-        "date","open","high","low","close","volume"
+        "date"
      )
 );
-
-ALTER TABLE "dogecoin" ADD CONSTRAINT "fk_dogecoin_date_open_high_low_close_volume" FOREIGN KEY("date", "open", "high", "low", "close", "volume")
-REFERENCES "dogeusd" ("date", "open", "high", "low", "close", "volume");
-
-
-SELECT * FROM dogecoin;
-SELECT * FROM dogeusd;
